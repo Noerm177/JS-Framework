25f3238c31b20e43b109c1d777d43646d1b6a004
Noerm177 <noerm177@gmail.com>
2023-01-04 00:25:04 -0600

    Add files via upload
    project upload

---
+++ src/stringReversalndex.js
@@ -0,0 +1,7 @@
+function solve(st, a, b) {
+  let strSubsctrated = st.substring(b, a)
+  console.log("codewars is " +strSubsctrated)
+  return strSubsctrated
+}
+
+module.exports = solve
---
+++ src/stringReversalndex.test.js
@@ -0,0 +1,9 @@
+const solve = require("./stringReversalndex")
+
+describe('Basic test', () => {
+
+  it('codewars = cawedors', () => {
+    expect(solve("codewars",1,5)).toBe("cawedors")
+  } )
+
+})
